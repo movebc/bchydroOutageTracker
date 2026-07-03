933388c3f698f86072cfce02b041c3e39b2afe57
underscore11code <minecrafter11mrt@gmail.com>
2026-07-02 18:53:38 -0700

    Handle script being called from outside the directory

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o errexit
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRAPE_ID=$(date +%s)
 DATA_DIR="/tmp/bchydroScrapes/$SCRAPE_ID"
 
@@ -12,6 +13,6 @@ curl -A movebc-bchydroOutageTracker/1.0 https://www.bchydro.com/power-outages/ap
 curl -A movebc-bchydroOutageTracker/1.0 https://www.bchydro.com/power-outages/app/outages-list-data-restored_14_days.json | jq . > $DATA_DIR/restored.json
 curl -A movebc-bchydroOutageTracker/1.0 https://www.bchydro.com/power-outages/app/outages-list-data-planned.json | jq . > $DATA_DIR/planned.json
 
-psql -v bch_scrape_timestamp=$SCRAPE_TIME -v bch_scrape_datadir=$DATA_DIR -f handleScrape.sql
+psql -v bch_scrape_timestamp=$SCRAPE_TIME -v bch_scrape_datadir=$DATA_DIR -f $SCRIPT_DIR/handleScrape.sql
 
 rm -rf $DATA_DIR
