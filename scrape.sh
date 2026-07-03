6231e971a2bd896fcbd74a87715adda3a115cccb
underscore11code <minecrafter11mrt@gmail.com>
2026-07-02 18:56:42 -0700

    Quiet curl

--- scrape.sh
+++ scrape.sh
@@ -9,9 +9,9 @@ mkdir -p $DATA_DIR
 
 SCRAPE_TIME=$(date -Iseconds)
 
-curl -A movebc-bchydroOutageTracker/1.0 https://www.bchydro.com/power-outages/app/outages-list-data-current.json | jq . > $DATA_DIR/current.json
-curl -A movebc-bchydroOutageTracker/1.0 https://www.bchydro.com/power-outages/app/outages-list-data-restored_14_days.json | jq . > $DATA_DIR/restored.json
-curl -A movebc-bchydroOutageTracker/1.0 https://www.bchydro.com/power-outages/app/outages-list-data-planned.json | jq . > $DATA_DIR/planned.json
+curl -A movebc-bchydroOutageTracker/1.0 -s https://www.bchydro.com/power-outages/app/outages-list-data-current.json | jq . > $DATA_DIR/current.json
+curl -A movebc-bchydroOutageTracker/1.0 -s https://www.bchydro.com/power-outages/app/outages-list-data-restored_14_days.json | jq . > $DATA_DIR/restored.json
+curl -A movebc-bchydroOutageTracker/1.0 -s https://www.bchydro.com/power-outages/app/outages-list-data-planned.json | jq . > $DATA_DIR/planned.json
 
 psql -v bch_scrape_timestamp=$SCRAPE_TIME -v bch_scrape_datadir=$DATA_DIR -f $SCRIPT_DIR/handleScrape.sql
 
